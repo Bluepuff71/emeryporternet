e9bddf6a0a9a179afe609638a2d4cf913dcb686c
Emery Porter <emeryporter99@gmail.com>
2021-03-04 16:35:05 -0600

    Hashing works but only for main tabs

--- js/main.js
+++ js/main.js
@@ -11,42 +11,46 @@ async function loadHtmlIntoDiv(html_file, div_id) {
     contentDiv.innerHTML = await fetchHtmlAsText(html_file);
 }
 
-async function loadHtmlIntoDivWithCallback(html_file, div_id, callback)
-{
-    await loadHtmlIntoDiv(html_file, div_id);
-    callback();
-}
-
-function loadTabs()
+async function RegisterTabs()
 {
     let url = location.href.replace(/\/$/, "");
 
+    let triggerTabList = [].slice.call(document.querySelectorAll('#mainTabs a'))
+    triggerTabList.forEach(function (triggerEl) {
+        let tabTrigger = new bootstrap.Tab(triggerEl)
+        triggerEl.addEventListener('click', function (event) {
+            event.preventDefault();
+            tabTrigger.show();
+            let newUrl;
+            const hash = triggerEl.getAttribute("href");
+            console.log(hash);
+            if(hash === "#about") {
+                newUrl = url.split("#")[0];
+            } else {
+                newUrl = url.split("#")[0] + hash;
+
+            }
+            newUrl += "/";
+            history.replaceState(null, null, newUrl);
+        })
+    })
+
     if (location.hash) {
         const hash = url.split("#");
+        await LoadTab(hash[1]);
+        console.log(hash);
         const triggerEl = document.querySelector('#mainTabs a[href="#'+hash[1]+'"]');
+        console.log(bootstrap.Tab.getInstance(triggerEl));
         bootstrap.Tab.getInstance(triggerEl).show() // Select tab by name
-        url = location.href.replace(/\/#/, "#");
+        //url = location.href.replace(/\/#/, "#");
         history.replaceState(null, null, url);
         setTimeout(() => {
             window.scrollTo(0,0);
         }, 400);
     }
+}
 
-    let triggerTabList = [].slice.call(document.querySelectorAll('#mainTabs a[data-bs-toggle="tab"]'))
-    triggerTabList.forEach(function (triggerEl) {
-        let tabTrigger = new bootstrap.Tab(triggerEl)
-
-        triggerEl.addEventListener('click', function (event) {
-            console.log("TESt");
-            let newUrl;
-            const hash = triggerEl.getAttribute("href");
-            if(hash === "#about") {
-                newUrl = url.split("#")[0];
-            } else {
-                newUrl = url.split("#")[0] + hash;
-            }
-            newUrl += "/";
-            history.replaceState(null, null, newUrl);
-        })
-    });
+async function LoadTab(tab_name)
+{
+    await loadHtmlIntoDiv("pages/" + tab_name + "/navbar.html", tab_name + "-navbar")
 }
