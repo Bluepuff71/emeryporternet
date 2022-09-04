6fa84558191218074d1111e213876cc02341c6a7
Emery Porter <emeryporter99@gmail.com>
2022-09-04 07:57:26 -0500

    added tests for ensure.ts

---
+++ src/test/utils/ensure.spec.ts
@@ -0,0 +1,22 @@
+import { describe, it, expect } from "vitest";
+import { ensure } from "src/utils/ensure";
+
+describe("ensure.ts", () => {
+    it("should throw error on undefined data", () => {
+        const data = undefined;
+
+        expect(() => ensure(data)).toThrowError("Ensure failed for Object.");
+    });
+
+    it("should throw error on null data", () => {
+        const data = null;
+
+        expect(() => ensure(data)).toThrowError("Ensure failed for Object.");
+    });
+
+    it("should return Nonnullable data", () => {
+        const data: object | undefined = {};
+
+        expect(ensure(data)).toBeTypeOf("object");
+    });
+});
